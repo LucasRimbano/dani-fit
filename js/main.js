da0ba4803aeaed83232687250f5df456c14cd58d
Lucas Rimbano <Lucasrimbano@gmail.com>
2026-02-28 02:27:52 -0300

    Inicializando

---
+++ js/main.js
@@ -0,0 +1,44 @@
+new fullpage('#fullpage', {
+  autoScrolling: true,
+  navigation: true,
+  licenseKey: 'gplv3-license',
+
+  fixedElements: '.topbar, footer',
+  paddingTop: '70px',
+  paddingBottom: '120px' // ajustalo al alto del footer en móvil
+ 
+});
+
+
+
+const form = document.getElementById("formContacto");
+
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const nombre = document.getElementById("nombre").value;
+  const email = document.getElementById("email").value;
+  const objetivo = document.getElementById("objetivo").value;
+  const mensaje = document.getElementById("mensaje").value;
+  const terminos = document.getElementById("terminos");
+
+    if (!terminos.checked) {
+      alert("Debés aceptar los términos.");
+      return;
+}
+
+  const texto = `Hola Dani! 💪
+  Nombre: ${nombre}
+  Email: ${email}
+  Objetivo: ${objetivo}
+  Mensaje: ${mensaje}`;
+
+  const numero = "5491100000000"; 
+
+  const url = `https://wa.me/${numero}?text=${encodeURIComponent(texto)}`;
+
+  window.open(url, "_blank");
+});
+
+
