c30221e6b6e2fd85b09919396b17b20eb363b8fe
Lucas Rimbano <Lucasrimbano@gmail.com>
2026-03-04 17:30:05 -0300

    Cambiando navbar agregando secion de ul con nav-pills , agregando boton para ir arriba de todo ,cambiando y acomodando css

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ new fullpage('#fullpage', {
   paddingTop: '70px',
   paddingBottom: '120px', // ajustalo al alto del footer en móvil
 
+ 
+
+  anchors: ['home', 'contacto', 'planes', 'nosotros'], 
+  menu: '#menuTop' ,
   
   afterLoad: function(origin, destination, direction) {
 
@@ -17,7 +21,7 @@ new fullpage('#fullpage', {
       void card.offsetWidth; // reinicia animación
       setTimeout(() => {
         card.classList.add('scale-up-center');
-      }, index * 150); // efecto escalonado
+      }, index * 150);
     });
     
     
@@ -55,7 +59,7 @@ form.addEventListener("submit", function(event) {
 
   window.open(url, "_blank");
 });
-
+document.addEventListener("DOMContentLoaded", () => {
 const btnTop = document.getElementById("Arribatop");
 
 if (btnTop) {
@@ -72,3 +76,21 @@ function irarriba() {
     });
   }
 }
+
+//saco el boton top en menu hamb//
+
+ const offcanvas = document.getElementById("menuMobile");
+
+
+  if(offcanvas && btnTop){
+
+    offcanvas.addEventListener("show.bs.offcanvas", () => {
+      btnTop.style.display = "none";
+    });
+
+    offcanvas.addEventListener("hidden.bs.offcanvas", () => {
+      btnTop.style.display = "block";
+    });
+
+  }
+});
